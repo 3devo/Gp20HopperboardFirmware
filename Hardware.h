476af8005440ff32805788b040cfeec2f6569b9c
Matthijs Kooijman <matthijs@stdin.nl>
2021-04-01 20:16:58 +0200

    Update pinout to v0.2 board

--- Hardware.h
+++ Hardware.h
@@ -13,12 +13,12 @@ static const uint32_t IRBOARD_SCL = PB10;
 static const uint32_t IRBOARD_SDA = PB11;
 
 // I2C1
-static const uint32_t IFACEBOARD_SCL = PA9;
-static const uint32_t IFACEBOARD_SDA = PA10;
+static const uint32_t IFACEBOARD_SCL = PB6;
+static const uint32_t IFACEBOARD_SDA = PB7;
 
 // I2C1
-static const uint32_t RS485_TX = PB6;
-static const uint32_t RS485_RX = PB7;
-static const uint32_t RS485_DE = PB3;
+static const uint32_t RS485_TX = PA9;
+static const uint32_t RS485_RX = PA10;
+static const uint32_t RS485_DE = PA12;
 
 static const uint32_t STATUS_PIN = PB9;
