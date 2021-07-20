38d1a201cc56c5612aed6c0ad14b30bf2642c6f0
Matthijs Kooijman <matthijs@stdin.nl>
2021-07-20 12:43:13 +0200

    Add more hardware info

--- src/Config.h
+++ src/Config.h
@@ -6,6 +6,7 @@
 
 #include <Arduino.h>
 
+// UART2
 static const uint32_t DEBUG_TX = PA2;
 
 // I2C2
@@ -16,12 +17,27 @@ static const uint32_t IRBOARD_SDA = PB11;
 static const uint32_t IFACEBOARD_SCL = PB6;
 static const uint32_t IFACEBOARD_SDA = PB7;
 
-// I2C1
+// UART1
 static const uint32_t RS485_TX = PA9;
 static const uint32_t RS485_RX = PA10;
 static const uint32_t RS485_DE = PA12;
 
 static const uint32_t STATUS_PIN = PB9;
+
+static const uint32_t MODE0 = PA5;
+static const uint32_t MODE1 = PA6;
+static const uint32_t MODE2 = PA7;
+
+static const uint32_t STEPPER_NSLEEP = PB13;
+static const uint32_t STEPPER_NENBL = PB1;
+static const uint32_t STEPPER_DIR = PB2;
+static const uint32_t STEPPER_STEP = PB0;
+static const uint32_t STEPPER_NFAULT = PB12;
+
+//I2C addresses
+static const uint32_t LED_DRIVER_ADDR = 0x01;
+static const uint32_t ADC_ADDR = 0x65;
+
 
 // Used for SET_ADDRESS, just use 0 to only respond to the
 // wildcard type.
